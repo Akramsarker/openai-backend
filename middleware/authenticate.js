f47beb96c7d5384ec519634ade90771fe4b78b3a
nasim145142 <nasim@bitstrapped.com>
2022-03-16 18:55:34 +0600

    Service account add in auth middleware

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,9 +1,11 @@
 var admin = require("firebase-admin");
-
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: serviceAccount.project_id,
+    clientEmail: serviceAccount.client_email,
+    privateKey: serviceAccount.private_key.replace(/\\n/g, "\n"),
+  }),
 });
 
 const authRoute = async (req, res, next) => {
