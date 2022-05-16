24ad5336d524173e010e372e416dd5ddc475c458
nasim145142 <nasim@bitstrapped.com>
2022-05-16 13:27:00 +0600

    Get course api refactor & course data structure update

--- api/course/v1/index.js
+++ api/course/v1/index.js
@@ -1,12 +1,7 @@
 const router = require("express").Router();
 const root = require("app-root-path");
-const htmlCss = require(`${root}/courses/videos/html-css.json`);
-const javascript = require(`${root}/courses/videos/html-css.json`);
-const vue = require(`${root}/courses/videos/html-css.json`);
-const git = require(`${root}/courses/videos/html-css.json`);
+const html = require(`${root}/courses/videos/html.json`);
 const mongo = require(`${root}/services/mongo-crud`);
-
-const authRoute = require(`${root}/middleware/authenticate`);
 
 const filterVideos = (object, hasPaid) => {
   const newObj = JSON.parse(JSON.stringify(object));
@@ -34,18 +29,18 @@ getCourse = async (req, res, next) => {
     const { course_name, username } = req.query;
     const hasPaid = await checkHasPaid(username, course_name);
     if (course_name === "html") {
-      filteredVideo = filterVideos(htmlCss, hasPaid);
-    } else if (course_name === "css") {
-      filteredVideo = filterVideos(css, hasPaid);
-    } else if (course_name === "javascript") {
-      filteredVideo = filterVideos(javascript, hasPaid);
-    } else if (course_name === "vue") {
-      filteredVideo = filterVideos(vue, hasPaid);
-    } else if (course_name === "git") {
-      filteredVideo = filterVideos(git, hasPaid);
+      filteredVideo = filterVideos(html, hasPaid);
+      // } else if (course_name === "css") {
+      //   filteredVideo = filterVideos(css, hasPaid);
+      // } else if (course_name === "javascript") {
+      //   filteredVideo = filterVideos(javascript, hasPaid);
+      // } else if (course_name === "vue") {
+      //   filteredVideo = filterVideos(vue, hasPaid);
+      // } else if (course_name === "git") {
+      //   filteredVideo = filterVideos(git, hasPaid);
     } else {
       const status = "NOT FOUND";
-      return res.status(404).json({ success: true, status });
+      return res.status(404).json({ success: false, status });
     }
     return res.status(200).json({ success: true, videos: filteredVideo });
   } catch (error) {
