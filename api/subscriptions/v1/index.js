dbeea2b9c64cac766f5f0a6b0ea751e2228256e3
moyen90 <moyen@connekt.studio>
2022-07-25 21:51:03 +0600

    discount api create

--- api/subscriptions/v1/index.js
+++ api/subscriptions/v1/index.js
@@ -5,70 +5,76 @@ const getMongoConnection = require(`${root}/services/mongo-connect`)
 
 const authRoute = require(`${root}/middleware/authenticate`);
 
-const addSubscription = async ({
-  subscriptions,
-  isRecurring,
-  interval,
-  course,
-  finalPrice,
-  discountedPrice,
-  percentage,
-  price,
-  coupon,
-  validFrom,
-}) => {
-  let validTill;
-  if (interval === "monthly") validTill = validFrom + 2592000000; // 1 month
-  else if (interval === "yearly") validTill = validFrom + 31536000000; // 12 month
-  else validTill = validFrom + 31536000000; // 12 month
-  let subscriptionObj = {
-    price: { price, finalPrice, discount: { amount: discountedPrice, percentage, coupon } },
-    validFrom,
-    validTill,
-    status: "pending",
-  };
-  if (isRecurring) {
-    return (subscriptions.recurring = { ...subscriptionObj, interval });
-  } else {
-    return (subscriptions.courses[`${course}`] = { ...subscriptionObj });
-  }
-};
-
-const getDiscount = async (db, coupon, price, validFrom) => {
-  if (!coupon) return { finalPrice: price, discountedPrice: 0, percentage: 0 };
-  const couponDetails = await mongo.fetchOne(db, "coupons", { coupon });
-  if (!couponDetails) throw new Error("invalid coupon code!");
-  else if (couponDetails.validTill < validFrom) throw new Error("Coupon code expired!");
-  const discountedPrice = (couponDetails.percentage / 100) * price;
-  const finalPrice = price - discountedPrice;
-  return { finalPrice, discountedPrice, percentage: couponDetails.percentage };
-};
 
 postSubscription = async (req, res, next) => {
   const { client, db } = await getMongoConnection();
   try {
-    const { isRecurring, price, interval, coupon, course, userId, username } = req.body;
+    const { price, interval, coupon, userId, username, finalPrice, discountedPrice, percentage } = req.body;
     const validFrom = new Date().getTime();
     let query = {};
     if (userId) query = { userId };
     if (username) query = { username };
+    let validTill;
+    if (interval === "monthly") validTill = validFrom + 2592000000; // 1 month
+    else if (interval === "yearly") validTill = validFrom + 31536000000; // 12 month
     let { subscriptions } = await mongo.fetchOne(db, "person", query);
-    const { error, finalPrice, discountedPrice, percentage } = await getDiscount(db, coupon, price, validFrom);
-    if (error) res.status(400).send({ message: error.message });
-    subscriptions = await addSubscription({
-      subscriptions,
-      isRecurring,
-      interval,
-      course,
-      finalPrice,
-      discountedPrice,
-      percentage,
-      price,
-      coupon,
+    const subscriptionObj = {
+      price: { price, finalPrice, discount: { amount: discountedPrice, percentage, coupon } },
       validFrom,
-    });
+      validTill,
+      status: "pending",
+    };
+    subscriptions.recurring = { ...subscriptionObj, interval };
     const isSubscriptionAdded = await mongo.updateOne(db, "person", query, { subscriptions });
     res.status(200).json({ success: isSubscriptionAdded });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message });
+  } finally {
+    await client.close();
+  }
+};
+
+postCourseSubscription = async (req, res, next) => {
+  const { client, db } = await getMongoConnection();
+  try {
+    const { price, coupon, course, userId, username, finalPrice, discountedPrice, percentage } = req.body;
+    const validFrom = new Date().getTime();
+    let query = {};
+    if (userId) query = { userId };
+    if (username) query = { username };
+    const validTill = validFrom + 31536000000; // 12 month
+    let { subscriptions } = await mongo.fetchOne(db, "person", query);
+    const subscriptionObj = {
+      price: { price, finalPrice, discount: { amount: discountedPrice, percentage, coupon } },
+      validFrom,
+      validTill,
+      status: "pending",
+    };
+    subscriptions.courses[`${course}`] = { ...subscriptionObj };
+    const isSubscriptionAdded = await mongo.updateOne(db, "person", query, { subscriptions });
+    res.status(200).json({ success: isSubscriptionAdded });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message });
+  } finally {
+    await client.close();
+  }
+};
+
+getDiscount = async (req, res, next) => {
+  const { client, db } = await getMongoConnection();
+  try {
+    const { coupon, price } = req.body;
+    const now = new Date().getTime();
+    if (!coupon) return { finalPrice: price, discountedPrice: 0, percentage: 0 };
+    const couponDetails = await mongo.fetchOne(db, "coupons", { coupon });
+    console.log(couponDetails);
+    if (!couponDetails) throw new Error("invalid coupon code!");
+    else if (couponDetails.validTill < now) throw new Error("Coupon code expired!");
+    const discountedPrice = (couponDetails.percentage / 100) * price;
+    const finalPrice = price - discountedPrice;
+    res.status(200).json({ success: true, discount: { finalPrice, discountedPrice, percentage: couponDetails.percentage } });
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: error.message });
@@ -109,5 +115,7 @@ hasPaid = async (req, res, next) => {
 
 
 router.post("/subscription", authRoute, postSubscription);
+router.post('/discount', authRoute, getDiscount);
+router.post('/course-subscription', authRoute, postCourseSubscription);
 router.put("/has-paid", authRoute, hasPaid);
 module.exports = router;
