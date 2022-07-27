e5d017292d8f6b799288b782c958af5d4cbc4ab8
nasimuddin01 <debater.nasim@gmail.com>
2022-07-27 18:38:36 +0600

    hasPaid function fix

--- api/subscriptions/v1/index.js
+++ api/subscriptions/v1/index.js
@@ -2,11 +2,9 @@ const router = require("express").Router();
 const root = require("app-root-path");
 const mongo = require(`${root}/services/mongo-crud`);
 const getMongoConnection = require(`${root}/services/mongo-connect`);
-const objectId = require('mongodb').ObjectID;
-
+const objectId = require("mongodb").ObjectID;
 
 const authRoute = require(`${root}/middleware/authenticate`);
-
 
 postSubscription = async (req, res, next) => {
   const { client, db } = await getMongoConnection();
@@ -68,12 +66,14 @@ getDiscount = async (req, res, next) => {
     const { coupon, price, course_name } = req.query;
     const now = new Date().getTime();
     if (!coupon) return { finalPrice: price, discountedPrice: 0, percentage: 0 };
-    const couponDetails = await mongo.fetchOne(db, "coupons", { coupon, course: course_name })
+    const couponDetails = await mongo.fetchOne(db, "coupons", { coupon, course: course_name });
     if (!couponDetails) throw new Error("invalid coupon code!");
     else if (couponDetails.validTill < now) throw new Error("Coupon code expired!");
     const discountedPrice = (couponDetails.percentage / 100) * price;
     const finalPrice = price - discountedPrice;
-    res.status(200).json({ success: true, discount: { finalPrice, discountedPrice, percentage: couponDetails.percentage } });
+    res
+      .status(200)
+      .json({ success: true, discount: { finalPrice, discountedPrice, percentage: couponDetails.percentage } });
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: error.message });
@@ -85,7 +85,19 @@ getDiscount = async (req, res, next) => {
 payment = async (req, res, next) => {
   const { client, db } = await getMongoConnection();
   try {
-    const { userId, username, fullName, email, course, courseTitle, price, finalPrice, discountedPrice, percentage, coupon } = req.body;
+    const {
+      userId,
+      username,
+      fullName,
+      email,
+      course,
+      courseTitle,
+      price,
+      finalPrice,
+      discountedPrice,
+      percentage,
+      coupon,
+    } = req.body;
     const validTill = new Date().getTime() + 600000; // 10 minutes
     let query = {};
     if (userId) query = { userId };
@@ -99,7 +111,7 @@ payment = async (req, res, next) => {
       course,
       courseTitle,
       fullName,
-      email
+      email,
     };
     const payment = await mongo.insertOne(db, "payment", paymentObj);
     res.status(200).json({ success: !!payment, sessionId: payment._id });
@@ -109,7 +121,7 @@ payment = async (req, res, next) => {
   } finally {
     await client.close();
   }
-}
+};
 
 getPaymentStatus = async (req, res, next) => {
   const { client, db } = await getMongoConnection();
@@ -123,21 +135,20 @@ getPaymentStatus = async (req, res, next) => {
   } finally {
     await client.close();
   }
-}
-
+};
 
 hasPaid = async (req, res, next) => {
   const { client, db } = await getMongoConnection();
   try {
-    const { username, id, course_name, is_recurring, success } = req.query;
+    const { username, id, course, isRecurring, success } = req.body;
     const person = await mongo.fetchOne(db, "person", { username });
     const payment = await mongo.fetchOne(db, "payment", { _id: objectId(id) });
-    if (success === "true") {
+    if (success) {
       if (is_recurring) {
-        person.subscriptions.recurring.status = "paid";
+        person.subscriptions.recurring.status = "active";
         payment.status = "success";
       } else {
-        person.subscriptions[`${course_name}`].status = "paid";
+        person.subscriptions[`${course}`].status = "active";
         payment.status = "success";
       }
     } else {
@@ -145,28 +156,25 @@ hasPaid = async (req, res, next) => {
         person.subscriptions.recurring.status = "error";
         payment.status = "error";
       } else {
-        person.subscriptions[`${course_name}`].status = "error";
+        person.subscriptions[`${course}`].status = "error";
         payment.status = "error";
       }
     }
     await mongo.updateOne(db, "payment", { _id: objectId(id) }, payment);
     const hasPaid = await mongo.updateOne(db, "person", { username }, person);
     res.status(200).json({ success: hasPaid });
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: error.message });
   } finally {
     await client.close();
   }
-
-}
-
+};
 
 router.post("/subscription", authRoute, postSubscription);
-router.get('/discount', getDiscount);
-router.post('/course-subscription', authRoute, postCourseSubscription);
-router.post('/payment', authRoute, payment);
-router.get('/payment/:paymentId', getPaymentStatus);
+router.get("/discount", getDiscount);
+router.post("/course-subscription", authRoute, postCourseSubscription);
+router.post("/payment", authRoute, payment);
+router.get("/payment/:paymentId", getPaymentStatus);
 router.put("/has-paid", hasPaid);
 module.exports = router;
